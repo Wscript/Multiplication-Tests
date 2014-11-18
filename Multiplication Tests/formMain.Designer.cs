8034c968f48e4a8e9e234ba0bbb311bbe4fb171f
Wscript <dick.li.sheng@gmail.com>
2014-11-18 09:43:38 +0800

    初版完成

---
+++ Multiplication Tests/formMain.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Multiplication_Tests
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelMinutes = new System.Windows.Forms.Label();
+            this.textBoxMinutes = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.labelEqual = new System.Windows.Forms.Label();
+            this.labelNumber2 = new System.Windows.Forms.Label();
+            this.labelSymbol = new System.Windows.Forms.Label();
+            this.labelNumber1 = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonStart);
+            this.groupBox1.Controls.Add(this.labelMinutes);
+            this.groupBox1.Controls.Add(this.textBoxMinutes);
+            this.groupBox1.Location = new System.Drawing.Point(30, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 118);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonStart.Location = new System.Drawing.Point(222, 30);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(128, 62);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "开始";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelMinutes
+            // 
+            this.labelMinutes.AutoSize = true;
+            this.labelMinutes.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelMinutes.Location = new System.Drawing.Point(90, 33);
+            this.labelMinutes.Name = "labelMinutes";
+            this.labelMinutes.Size = new System.Drawing.Size(116, 48);
+            this.labelMinutes.TabIndex = 4;
+            this.labelMinutes.Text = "分钟";
+            // 
+            // textBoxMinutes
+            // 
+            this.textBoxMinutes.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxMinutes.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBoxMinutes.Location = new System.Drawing.Point(24, 30);
+            this.textBoxMinutes.MaxLength = 2;
+            this.textBoxMinutes.Name = "textBoxMinutes";
+            this.textBoxMinutes.Size = new System.Drawing.Size(60, 62);
+            this.textBoxMinutes.TabIndex = 0;
+            this.textBoxMinutes.Text = "10";
+            this.textBoxMinutes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxMinutes.TextChanged += new System.EventHandler(this.textBoxMinutes_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxResult);
+            this.groupBox2.Controls.Add(this.labelEqual);
+            this.groupBox2.Controls.Add(this.labelNumber2);
+            this.groupBox2.Controls.Add(this.labelSymbol);
+            this.groupBox2.Controls.Add(this.labelNumber1);
+            this.groupBox2.Location = new System.Drawing.Point(30, 164);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(371, 113);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxResult.Location = new System.Drawing.Point(264, 37);
+            this.textBoxResult.MaxLength = 2;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(86, 62);
+            this.textBoxResult.TabIndex = 9;
+            this.textBoxResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxResult.TextChanged += new System.EventHandler(this.textBoxResult_TextChanged);
+            // 
+            // labelEqual
+            // 
+            this.labelEqual.AutoSize = true;
+            this.labelEqual.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelEqual.Location = new System.Drawing.Point(214, 40);
+            this.labelEqual.Name = "labelEqual";
+            this.labelEqual.Size = new System.Drawing.Size(44, 48);
+            this.labelEqual.TabIndex = 8;
+            this.labelEqual.Text = "=";
+            // 
+            // labelNumber2
+            // 
+            this.labelNumber2.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelNumber2.Location = new System.Drawing.Point(140, 40);
+            this.labelNumber2.Name = "labelNumber2";
+            this.labelNumber2.Size = new System.Drawing.Size(68, 48);
+            this.labelNumber2.TabIndex = 7;
+            this.labelNumber2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelSymbol
+            // 
+            this.labelSymbol.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelSymbol.Location = new System.Drawing.Point(90, 40);
+            this.labelSymbol.Name = "labelSymbol";
+            this.labelSymbol.Size = new System.Drawing.Size(44, 48);
+            this.labelSymbol.TabIndex = 6;
+            // 
+            // labelNumber1
+            // 
+            this.labelNumber1.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelNumber1.Location = new System.Drawing.Point(16, 40);
+            this.labelNumber1.Name = "labelNumber1";
+            this.labelNumber1.Size = new System.Drawing.Size(68, 48);
+            this.labelNumber1.TabIndex = 5;
+            this.labelNumber1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelTime
+            // 
+            this.labelTime.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelTime.ForeColor = System.Drawing.Color.Red;
+            this.labelTime.Location = new System.Drawing.Point(30, 307);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(142, 50);
+            this.labelTime.TabIndex = 2;
+            this.labelTime.Text = "00:00";
+            this.labelTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelScore
+            // 
+            this.labelScore.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelScore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.labelScore.Location = new System.Drawing.Point(310, 307);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(70, 50);
+            this.labelScore.TabIndex = 3;
+            this.labelScore.Text = "00";
+            this.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 379);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "formMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "乘法口诀测试";
+            this.Load += new System.EventHandler(this.formMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelMinutes;
+        private System.Windows.Forms.TextBox textBoxMinutes;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label labelEqual;
+        private System.Windows.Forms.Label labelNumber2;
+        private System.Windows.Forms.Label labelSymbol;
+        private System.Windows.Forms.Label labelNumber1;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Multiplication Tests/formMain.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplication_Tests
+{
+    public partial class formMain : Form
+    {
+        static int timerTickCounter = 0;
+        static Random randomizer = new Random();
+
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void formMain_Load(object sender, EventArgs e)
+        {
+            textBoxMinutes.Enabled = true;
+            textBoxMinutes.Focus();
+            textBoxMinutes.Text = "1";
+            textBoxResult.Enabled = false;
+            this.AcceptButton = buttonStart;
+            labelSymbol.Text = "x";
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            int intMinutes = 0;
+            string strRemainMinutes = "";
+            string strRemainSeconds = "";
+            bool isNumber = int.TryParse(textBoxMinutes.Text, out intMinutes);
+
+            timerTickCounter = timerTickCounter + 1;
+
+            int intRemainMinutes = (intMinutes * 60 - timerTickCounter) / 60;
+            int intRemainSeconds = (intMinutes * 60 - timerTickCounter) % 60;
+            if (intRemainMinutes < 10)
+            {
+                strRemainMinutes = "0" + intRemainMinutes.ToString();
+            }
+            else
+            {
+                strRemainMinutes = intRemainMinutes.ToString();
+            }
+            if (intRemainSeconds < 10)
+            {
+                strRemainSeconds = "0" + intRemainSeconds.ToString();
+            }
+            else
+            {
+                strRemainSeconds = intRemainSeconds.ToString();
+            }
+
+            labelTime.Text = strRemainMinutes + ":" + strRemainSeconds;
+
+            if (timerTickCounter / 60 == intMinutes)
+            {
+                timer.Stop();
+                timerTickCounter = 0;           //停止计时器，并将激活次数清零
+                textBoxMinutes.Enabled = true;
+                textBoxResult.Enabled = false;
+                buttonStart.Enabled = true;
+                textBoxMinutes.Focus();
+
+            }
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            int intMinutes = 0;
+            if (int.TryParse(textBoxMinutes.Text, out intMinutes))
+            {
+                labelTime.Text = intMinutes.ToString() + ":00";
+                timer.Interval = 1000;
+                timer.Start();
+                textBoxMinutes.Enabled = false;
+                textBoxResult.Enabled = true;
+                textBoxResult.Text = "";
+                textBoxResult.Focus();
+                labelScore.Text = "0";
+                buttonStart.Enabled = false;
+                NewQuiz();
+            }
+        }
+
+        private void textBoxMinutes_TextChanged(object sender, EventArgs e)
+        {
+            int intMinutes = 0;
+            if (int.TryParse(textBoxMinutes.Text, out intMinutes))
+            {
+                labelTime.Text = intMinutes.ToString() + ":00";
+            }
+        }
+
+        private void textBoxResult_TextChanged(object sender, EventArgs e)
+        {
+            int intNumber1, intNumber2, intResult, intScore, intAnswer;
+            if (int.TryParse(textBoxResult.Text, out intResult))
+            {
+                intNumber1 = System.Convert.ToInt32(labelNumber1.Text);
+                intNumber2 = System.Convert.ToInt32(labelNumber2.Text);
+                intAnswer = intNumber1 * intNumber2;
+                if (intAnswer == intResult)
+                {
+                    intScore = System.Convert.ToInt32(labelScore.Text);
+                    intScore = intScore + 1;
+                    labelScore.Text = intScore.ToString();
+                    textBoxResult.Text = "";
+                    NewQuiz();
+                }
+            }
+        }
+
+        private void NewQuiz()
+        {
+            int intNumber1, intNumber2;
+
+            //随机生成2个1-9的整数
+            intNumber1 = randomizer.Next(9) + 1;
+            intNumber2 = randomizer.Next(9) + 1;
+            if (intNumber1 < intNumber2)
+            {
+                labelNumber1.Text = intNumber1.ToString();
+                labelNumber2.Text = intNumber2.ToString();
+            }
+            else
+            {
+                labelNumber1.Text = intNumber2.ToString();
+                labelNumber2.Text = intNumber1.ToString();
+            }
+        }
+    }
+}
